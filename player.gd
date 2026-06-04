4138052c07461482c1a6dbe0ddf64bfedc235bb0
Rift <61116182+Riftaricus@users.noreply.github.com>
2026-06-04 08:46:49 +0200

    Added movement

--- player.gd
+++ player.gd
@@ -1,31 +1,53 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 16000
-var screen_size
-const GRAVITY = 1
-var velocity = Vector2.ZERO
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	screen_size = get_viewport_rect().size
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var air_jumps = 1
+var current_air_jumps = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 5
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 5
-	if Input.is_action_pressed("jump"):
-		velocity.y -= 5
+var coyote_timer = 0.0
+const COYOTE_TIME_THRESHOLD = 0.1
+
+var jump_buffer_timer = 0.0
+const JUMP_BUFFER_TIMER_THRESHOLD = 0.1
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity*delta
+	else:
+		current_air_jumps = air_jumps
+		coyote_timer = COYOTE_TIME_THRESHOLD
 	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
+	if coyote_timer > 0:
+		coyote_timer -= delta
+	if jump_buffer_timer > 0:
+		jump_buffer_timer -= delta
 		
-	velocity.y -= gravity
-	if velocity.x > 0:
-		velocity.x -= 0.5
-	elif velocity.x < 0:
-		velocity.x += 0.5
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = JUMP_BUFFER_TIMER_THRESHOLD
+	if jump_buffer_timer > 0:
+		if is_on_floor() or coyote_timer > 0:
+			velocity.y = JUMP_VELOCITY
+			jump_buffer_timer = 0
+			coyote_timer = 0
+		elif current_air_jumps > 0:
+			velocity.y	 = JUMP_VELOCITY * 0.8
+			current_air_jumps -= 1
+			jump_buffer_timer = 0
+	
+	var direction = Input.get_axis("move_left", "move_right") # "move_left" & "move_right" from InputMap
+			
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * 2.0 * delta)
+		
+		if $Sprite2D:
+			$Sprite2D.flip_h = (direction < 0)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * 2.0 * delta)
+	move_and_slide()
+
+		
+		
