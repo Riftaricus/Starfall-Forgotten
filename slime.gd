a57178f8ce651ea5b6102c24f0a6a34c1f2462fd
M3v_ <max@m3v.dev>
2026-06-11 09:33:49 +0200

    started working on Slime movement

--- slime.gd
+++ slime.gd
@@ -1,13 +1,22 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+var vel_multiplier = 5
+var player
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
-	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
-	$AnimatedSprite2D.animation = mob_types[0]
-	print(mob_types)
-	$AnimatedSprite2D.play()
+	player = get_node("/root/Game/Player")
+	$AnimatedSprite2D.play("walk")
 
+func _physics_process(delta: float) -> void:
+	var on_floor = is_on_floor()
+	if not on_floor:
+		velocity.y += gravity * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if player:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * 3
+		move_and_slide()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
